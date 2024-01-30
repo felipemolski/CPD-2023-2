ebf0de5b574da41c9df85ed35f57158d892388bc
Felipe Molski <felipe.molski@gmail.com>
2024-01-30 11:06:07 -0300

    30/01/2024 11:05

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-#import csv
-#import re
 import time
-
 
 class PlayerData:
     def __init__(
@@ -14,6 +11,7 @@ class PlayerData:
         club,
         league,
         ratings=None,
+        tags=None
     ):
         self.id = id
         self.name_short = name_short
@@ -30,27 +28,23 @@ class PlayerData:
             else 0.0
         )
         self.user_ratings = {}  # Adiciona o atributo user_ratings
+        self.tags = tags or []  # Adiciona o atributo tags
 
+class Node:
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.children = []
 
+    def add_child(self, id):
+        self.children.append(id)
+
+    def get_children_with_prefix(self, prefix):
+        children = []
+        for child in self.children:
+            if child.prefix.startswith(prefix):
+                children.append(child)
+        return children
             
-class Regex:
-    def __init__(self, pattern):
-        self.pattern = pattern
-
-    def match(self, s):
-        matches = []
-        pos = 0
-        while True:
-            match = s.find(self.pattern, pos)
-            if match == -1:
-                break
-
-            matches.append(match)
-            pos = match + len(self.pattern)
-
-        return matches
-
-
 def finditer(pattern, s):
     matches = []
     pos = 0
@@ -109,9 +103,9 @@ def build_prefix_tree(players_csv):
                 prefix = name_long[:i + 1]
 
                 if prefix not in prefix_tree:
-                    prefix_tree[prefix] = []
+                    prefix_tree[prefix] = Node(prefix)
 
-                prefix_tree[prefix].append(row[0])
+                prefix_tree[prefix].add_child(row[0])
 
     return prefix_tree
 
@@ -185,8 +179,9 @@ def build_player_data_hash(players_csv, ratings_csv, tags_csv):
         next(reader_t, None)  # Skip header
 
         for row_t in reader_t:
-            if row_t[0] == str(id):
-                player_data.tags = [tag for tag in row_t[1:]]
+            if player_data_hash.get(str(row_r[1])):
+                player_data_hash[str(row_r[1])].tags.append(str(row_t[2]))
+                #print(f"{player_data_hash[str(row_r[1])].name_long} {player_data_hash[str(row_r[1])].tags}")
 
     end_time_tags = time.perf_counter()
     print(f"Tempo de carregamento de tags.csv: {end_time_tags - start_time_tags:.2f} segundos")
@@ -194,8 +189,6 @@ def build_player_data_hash(players_csv, ratings_csv, tags_csv):
     total_time = time.perf_counter() - start_time_total
     print(f"Tempo de carregamento total: {total_time:.2f} segundos")
     
-    #print(f"{player_data_hash['257936'].rating}")
-
     return player_data_hash
 
 
@@ -270,6 +263,34 @@ def top_query(position, player_data_hash, n):
             )
     else:
         print(f"Não há jogadores na posição {position} com pelo menos 1000 avaliações.")
+        
+        
+        
+def tag_query(tags, player_data_hash):
+    players = []
+    player_ids_added = set()  # Cria um conjunto vazio para armazenar os ids dos jogadores adicionados
+
+    for id, player_data in player_data_hash.items():
+        for tag in tags:
+            if tag.strip().strip('"') not in player_data.tags:
+                break
+        else:
+            players.append((player_data.rating, id))
+            player_ids_added.add(id)  # Adiciona o id do jogador ao conjunto para evitar duplicações
+
+    players.sort(key=lambda rating_id: rating_id[0], reverse=True)
+
+    if players:
+        print(f"{'sofifa_id':<10} {'short_name':<20} {'long_name':<45} {'player_positions':<20} {'tags':<20} {'rating':<12} {'count':<6}")
+        for rating, id in players:
+            player_data = player_data_hash[id]
+            print(
+                f"{id:<10} {player_data.name_short:<20} {player_data.name_long:<45} {','.join(player_data.positions):<20} {','.join(player_data.tags):<20} {player_data.rating:<12.6f} {player_data.rating_count:<6}"
+            )
+    else:
+        print(f"Não há jogadores associados às tags {tags}.")
+
+
 
 
 def main():
@@ -298,12 +319,16 @@ def main():
             try:
                 print(f"{query.split(' ')}")
                 starts, n, position = query.split(" ", maxsplit=3)
-            #print(f"{n} {position}")
-            #position = position.strip().strip("'").strip('"')
                 n = int(n.strip())
                 top_query(position, player_data_hash, n)
             except (ValueError, IndexError):
-                print(f"Formato de consulta inválido: {query} {ValueError} {IndexError}")
+                print(f"Formato de consulta inválido: {query}")
+        elif query.startswith("tags "):
+            try:
+                tags = query[5:].split(" ")
+                tag_query(tags, player_data_hash)
+            except (ValueError, IndexError):
+                print(f"Formato de consulta inválido: {query}")
         else:
             print("Consulta inválida")
 
